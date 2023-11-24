e8b163acf07f8ecb8c7e7caed8bb5a6dea32dbc3
wambui-g <grayciekamau03@gmail.com>
2023-11-23 23:29:06 -0800

    6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A simple Flask web application"""
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -42,19 +42,13 @@ def number_page(n):
 @app.route('/number_template/<int:n>')
 def number_template(n):
     """The number_template page"""
-    ctxt = {
-        'n': n
-    }
-    return render_template('5-number.html', **ctxt)
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
     """The number_odd_or_even page"""
-    ctxt = {
-        'n': n
-    }
-    return render_template('6-number_odd_or_even.html', **ctxt)
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == '__main__':
